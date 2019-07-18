676e15a494545204726e1968c503618834ed5100
annaescalada <annaescalada@gmail.com>
2019-07-18 11:43:20 +0200

    First commit

--- recipes.js
+++ recipes.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+
 const data = require('./data.js');
+const Recipe = require('./models/Recipe.js');
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -8,3 +9,32 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+const createOneRecipe = async () => {
+  try {
+    const response = await Recipe.create({
+      title: 'Brocoli con patatas',
+      level: 'UltraPro Chef',
+      ingredients: ['brócoli', 'patatas', 'ajo'],
+      cuisine: 'veggie',
+      dishType: 'Dish',
+      duration: 30,
+      creator: 'Anna'
+    });
+    console.log(response.title);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addManyRecipes = async (data) => {
+  try {
+    const response = await Recipe.insertMany(data);
+    response.forEach(recipe => console.log(recipe.title));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+createOneRecipe();
+addManyRecipes(data);
